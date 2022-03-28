c9048685878e0090edb3e084d86bc667a5aff7fc
Edson Mier <edson_mier28@hotmail.com>
2022-03-28 01:22:21 -0600

    update: mobile complete

--- dexnav.js
+++ dexnav.js
@@ -32,4 +32,19 @@ function searchOnClick(){
   location.replace("./navigation.html");
 }
 
+  document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },1000);
+  }
+}
 
+function goBack(){
+  location.replace("./index.html");
+}
